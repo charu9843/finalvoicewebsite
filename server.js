bd6b076032a55e6265d819edb478fb299cfcae80
charu9843 <114791511+charu9843@users.noreply.github.com>
2025-08-16 20:57:01 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,8 @@ const archiver = require('archiver');
 require('dotenv').config();
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
+
 
 // OpenAI API setup
 const openai = new OpenAI({
